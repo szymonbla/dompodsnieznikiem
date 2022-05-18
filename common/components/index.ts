076449da5976977b24099ec7fc0a4caab0f52f78
Kuchar <sz.blazynski@gmail.com>
2022-05-18 15:07:10 +0200

    AB#70 Create gallery slide item

--- common/components/index.ts
+++ common/components/index.ts
@@ -2,3 +2,4 @@ export * from './NavLink';
 export * from './BannerImageComponent';
 export * from './Testimonials';
 export * from './IconWithValue';
+export * from './GalleryItem';
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 import { BaseLayout } from 'layouts/BaseLayout';
 import { Banner, AboutHome } from 'features';
-import { Testimonials } from 'common/components';
+import { Testimonials, GalleryItem } from 'common/components';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +10,12 @@ const Home: NextPage = () => {
       <Banner />
       <Testimonials />
       <AboutHome />
+      <GalleryItem
+        src="v1652877224/Gallery/dining-room.jpg"
+        alt="Dining Room"
+        slideTitle="Dining Room"
+        slideDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque, libero eget pulvinar volutpat, ligula eros consectetur enim, sit amet condimentum ligula elit eu tortor. Nullam consectetur erat et neque ornare vulputate"
+      />
     </BaseLayout>
   );
 };
