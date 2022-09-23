24b9c19647680740f36d60ae3305c3540afbbd49
Kuchar <sz.blazynski@gmail.com>
2022-09-23 17:27:05 +0200

    Implement better security

--- common/constants/firebaseConfig.ts
+++ common/constants/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCChKR9kyCpfIMzfQtbIDFySaN7t2L9sSU',
-  authDomain: 'podsnieznikiem-c0743.firebaseapp.com',
-  projectId: 'podsnieznikiem-c0743',
-  storageBucket: 'podsnieznikiem-c0743.appspot.com',
-  messagingSenderId: '1004213765196',
-  appId: '1:1004213765196:web:dda440a11cc5c9b6fb60e9',
-  measurementId: 'G-FCH655ZKPC'
+  apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+  authDomain: `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}`,
+  projectId: `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+  storageBucket: `${process.env.NEXT_PUBLIC_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.NEXT_PUBLIC_APP_ID}`,
+  measurementId: `${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`
 };
 
 export const getGoogleAnalytics = () => {
